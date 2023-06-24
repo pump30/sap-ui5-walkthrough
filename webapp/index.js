ac6443a719e4f2deafa8b9f09617a1474041c388
Dyland <dyland.zhang@sap.com>
2023-06-24 18:03:23 +0800

    controls

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-
-], function () {
+    "sap/m/Text"
+], function (Text) {
     "use strict";
 
-    alert("UI5 is ready to go");
+    new Text({
+        text: "Hello UI5!"
+    }).placeAt("content")
 });
