125effd189a05e1e990e8dcda66a58089c99de4f
Dyland <dyland.zhang@sap.com>
2023-06-25 23:18:33 +0800

    fragment

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,16 +1,39 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function(Controller, MessageToast){
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
-        onShowHello: function(){
+        onShowHello: function () {
             //read msg from i18n model
             var oBundle = this.getView().getModel("i18n").getResourceBundle();
             var sRecipient = this.getView().getModel().getProperty("/recipient/name");
             var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-        
+
             MessageToast.show(sMsg);
+        },
+
+        onOpenDialog: function () {
+            var oView = this.getView();
+
+            // create the dialog lazily
+            if (!this.byId('helloDialog')) {
+                // load asynchronous XML fragment
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "sap.ui.demo.walkthrough.view.HelloDialog",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                })
+            } else {
+                this.byId("helloDialog").open();
+            }
+        },
+        onCloseDialog: function () {
+            this.byId("helloDialog").close();
         }
     })
 });
